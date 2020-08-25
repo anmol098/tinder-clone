207883d8ebca62903be7c3c5d2605577f1bffd9f
Anmol Pratap Singh <63726491+misteranmol@users.noreply.github.com>
2020-08-25 18:34:57 +0530

    forgot to share

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  chainWebpack: (config) => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'Tinder-Clone-Pinder';
+          args[0].meta = {viewport: 'width=device-width,initial-scale=1,user-scalable=no'};
+
+          return args;
+        })
+  }
+}
